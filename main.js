7059fd6eeeba7a3dd1a5bdf36dabfabee2580418
Assiz_Carlos <141234828+AssizCarlos1@users.noreply.github.com>
2024-05-09 11:44:11 -0300

    save

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-<p id="valorVariavel"></p>
-
-
-var elementoParaExibirValor = document.getElementById("valorVariavel");
 var nome = console.log(prompt('Informe seu nome:'));
 
-elementoParaExibirValor.textContent = "O valor da variável é: " + nome;
+console.log('Bem vindo ao Site ${nome}');
